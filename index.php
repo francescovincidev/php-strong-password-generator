105e78ad3480657f1b08fafbca1be91bc536b41c
francescovincidev <francescovinci2000@gmail.com>
2023-05-06 13:35:14 +0200

    milestone 2

--- index.php
+++ index.php
@@ -9,24 +9,7 @@ $characters = [
     "spacials" => ["!#$%&'()*+,-./:;<=>?@[]^_"]
 ];
 
-// verifichiamo se $_GET['pass_length'] ha un valore
-if (isset($_GET['pass_length'])) {
-
-    // diamo a p_length il valore intager
-    $p_length = (int)$_GET['pass_length'];
-
-    // finchè la password è più corta di p_length
-    while (strlen($password) < $p_length) {
-        // prendiamo una chiave casuale tra letters numbers e specials
-        $random_key = array_rand($characters);
-        // prendiamo causalmente uno dei caratteri della chiave scelta
-        $random_chars = $characters[$random_key][0][rand(0, strlen($characters[$random_key][0]) - 1)];
-        //    aggiungiamo il carattere a password
-        $password .= $random_chars;
-    };
-
-    echo htmlspecialchars($password);
-};
+require "functions/functions.php";
 
 
 ?>
